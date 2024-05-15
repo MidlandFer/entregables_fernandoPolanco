588f77de6ff7da04d28e7c609e935b6d1cccc723
MidlandFer <ferpolanco83@gmail.com>
2024-05-15 20:42:11 -0300

    finalizando por tercera vez...

--- numArrayMax.ts
+++ numArrayMax.ts
@@ -1,31 +1,43 @@
-/*Encontrar el elemento más grande del arreglo
+/*Encontrar el elemento mas grande del arreglo
 
 Dado el siguiente arreglo
 [4,7,9,3,1,45,67,23,29,78,11,16]
-- Crear un programa que encuentre cuál es el número
-más grande del arreglo e imprimirlo por consola
-- Almacenar el número más grande en una variable
-global y pasarlo a una función para determinar si el
-número es par o impar
+- Crear un programa que encuentre cual es el numero
+mas grande del arreglo e imprimirlo por consola
+- Almacenar el numero mas grande en una variable
+global y pasarlo a una funcion para determinar si el
+numero es par o impar
 */
-// Definir el arreglo
-const arr: number[] = [4,7,9,3,1,45,67,23,29,78,11,16];
+// Definicion del arreglo
+const array: number[] = new Array (12);
+array[0] = 4;
+array[1] = 7;
+array[2] = 9;
+array[3] = 3;
+array[4] = 1;
+array[5] = 45;
+array[6] = 67;
+array[7] = 23;
+array[8] = 29;
+array[9] = 78;
+array[10] = 11;
+array[11] = 16;
 
-// Variable global para almacenar el número más grande
-let maximo: number = arr[0];
+// Variable global para almacenar el numero mas grande
+let numMaximo: number = array[0];
 
-// Función para encontrar el número más grande
-function encontrarMaximo(arreglo: number[]): number {
-    let max = arreglo[0];
-    for (let i = 1; i < arreglo.length; i++) {
-        if (arreglo[i] > max) {
-            max = arreglo[i];
+// Funcion para encontrar el numero mas grande
+function arrayMax(array: number[]): number {
+    let max = array[0];
+    for (let cont = 1; cont < array.length; cont++) {
+        if (array[cont] > max) {
+            max = array[cont];
         }
     }
     return max;
 }
 
-// Función para determinar si un número es par o impar
+// Funcion para determinar si un numero es par o impar
 function determinarParImpar(numero: number): void {
     if (numero % 2 === 0) {
         console.log(`${numero} es un número par.`);
@@ -35,7 +47,7 @@ function determinarParImpar(numero: number): void {
     }
 }
 
-// Función para dibujar línea
+// Funcion para dibujar linea
 function dibujarLinea(){
     let linea:string =" ";
 
@@ -47,12 +59,12 @@ function dibujarLinea(){
     return linea;
 }
 
-// Encontrar el número más grande
-maximo = encontrarMaximo(arr);
-console.log(`El número más grande del arreglo es: ${maximo}`);
+// Encontrar el numero mayor
+numMaximo = arrayMax(array);
+console.log(`El numero mayor del arreglo es: ${numMaximo}`)
 
-// Determinar si el número más grande es par o impar
-determinarParImpar(maximo);
+// Determinar si el numero mayor es par o impar
+determinarParImpar(numMaximo);
 
 //Dibujar linea
 dibujarLinea();
