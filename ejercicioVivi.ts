a5a8f4cf9538bfe4a9489bcc3fd9405a9b4e8fa7
MidlanFer <ferpolanco83@gmail.com>
2024-06-02 22:37:57 -0300

    falta

--- ejercicioVivi.ts
+++ ejercicioVivi.ts
@@ -12,3 +12,19 @@ Lista a modo de ejemplo: [1,5,4,6,8,7,6]
 import * as readlineSync from "readline-sync";
 
 let lista: number[] = [1, 5, 5, 6, 8, 7, 6];
+
+// Variable global para almacenar el numero mas grande
+let numMaximo: number = lista[0];
+
+// Funcion para encontrar el numero mas grande
+function listaMax(lista: number[]): number {
+    let max = lista[0];
+    for (let cont = 1; cont < lista.length; cont++) {
+        if (lista[cont] > max) {
+            max = lista[cont];
+        }
+    }
+    return max;
+
+
+}
